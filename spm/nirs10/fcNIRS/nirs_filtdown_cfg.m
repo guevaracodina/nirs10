590e39309c422cf176a844e743c0df03de08fa2e
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-12-03 02:18:45 +0000

    // EGC nirs_extract_roi_time_series_cfg.m - Fixed problems when loading nirs10 toolbox

--- spm/nirs10/fcNIRS/nirs_filtdown_cfg.m
+++ spm/nirs10/fcNIRS/nirs_filtdown_cfg.m
@@ -1,5 +1,5 @@
 function filtdown1 = nirs_filtdown_cfg
-% Graphical interface configuration function for ioi_filtdown_run
+% Graphical interface configuration function for nirs_filtdown_run
 %_______________________________________________________________________________
 % Copyright (C) 2010 LIOM Laboratoire d'Imagerie Optique et Moléculaire
 %                    École Polytechnique de Montréal
