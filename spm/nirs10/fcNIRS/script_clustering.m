4740ace2668255f6e746aaac08889276ae845446
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-12-11 22:02:44 +0000

    // EGC nirs_clusterdata.m - computes pdist, linkage, and cluster steps. nirs_clustergram.m - creates a dendrogram and heat map on the same figure. nirs_get_colormap.m - creates different colormaps script_clustering.m - clustering example

--- spm/nirs10/fcNIRS/script_clustering.m
+++ spm/nirs10/fcNIRS/script_clustering.m
@@ -1,4 +1,4 @@
-% Script to read NIRS data
+%% Clustering/linkage script with NIRS data
 %_______________________________________________________________________________
 % Copyright (C) 2013 LIOM Laboratoire d'Imagerie Optique et Moléculaire
 %                    École Polytechnique de Montréal
@@ -8,12 +8,7 @@ clear; close all; clc
 % Le cas 104MAL est extrêmement inhabituel. Il ne pourra pas être utilisé
 % pour les analyses.
 % NIRSmat = 'F:\Edgar\Data\NIRS\epiNIRS_data\epiNIRS_Processed\epiNIRS\epi104MAL\dataSPMa\coreg\NIRS.mat';
-% epi140GG 1st test, nothing special
-% NIRSmat = 'F:\Edgar\Data\NIRS\epiNIRS_data\epiNIRS_Processed\epiNIRS\epi140GG\dataSPMa\coreg\NIRS.mat';
-% epi127SD Good optodes coverage
-% NIRSmat = 'F:\Edgar\Data\NIRS\epiNIRS_data\epiNIRS_Processed\epiNIRS\epi127SD\dataSPMa\coreg\NIRS.mat';
-% epi143MDM  Good optodes coverage
-NIRSmat = 'F:\Edgar\Data\NIRS\epiNIRS_data\epiNIRS_Processed\epiNIRS\epi143MDM\dataSPMa\coreg\NIRS.mat';
+NIRSmat = 'F:\Edgar\Data\NIRS\epiNIRS_data\epiNIRS_Processed\epiNIRS\epi140GG\dataSPMa\coreg\NIRS.mat';
 load(NIRSmat)
 
 %% Read all files from the 4th processing level ODtoHbOHbR
@@ -22,22 +17,6 @@ load(NIRSmat)
 % 3) normalize_baseline
 % 4) ODtoHbOHbR
 preProcStep = 4;
-nSessions = numel(NIRS.Dt.fir.pp(preProcStep).p);
-dataNIRS = [];
-for iFiles = 1:nSessions,
-    % currentData [nChannels x nTimePoints]
-    currentData = fopen_NIR(NIRS.Dt.fir.pp(preProcStep).p{iFiles}, NIRS.Cf.H.C.N);
-    % dataNIRS [nChannels x (nTimePoints x nSessions)]
-    dataNIRS = [dataNIRS, currentData];
-end
-% Time vector
-t = linspace(0, (size(dataNIRS,2)-1)/NIRS.Cf.dev.fs , size(dataNIRS,2));
-
-%% Plot data
-h = figure; set(h,'color','w')
-set(h,'Name',sprintf('Pre-processing step: %s', NIRS.Dt.fir.pp(preProcStep).pre))
-subplot(211)
-fprintf('Wavelength: %dnm & %dnm\n',NIRS.Cf.dev.wl)
 % switch chromophore
 %     case 1
 %         hb = 'HbO';
@@ -47,21 +26,34 @@ fprintf('Wavelength: %dnm & %dnm\n',NIRS.Cf.dev.wl)
 %         hb = 'HbT';
 % end
 hb = 2;
-plot(t, dataNIRS(NIRS.Cf.H.C.wl == hb,:)');
+nSessions = numel(NIRS.Dt.fir.pp(preProcStep).p);
+dataNIRS = [];
+for iFiles = 1:nSessions,
+    % currentData [nChannels x nTimePoints]
+    currentData = fopen_NIR(NIRS.Dt.fir.pp(preProcStep).p{iFiles}, NIRS.Cf.H.C.N);
+    % dataNIRS [nChannels x (nTimePoints x nSessions)]
+    dataNIRS = [dataNIRS, currentData];
+end
+dataNIRS = dataNIRS(NIRS.Cf.H.C.wl == hb,:);
+% Time vector
+t = linspace(0, (size(dataNIRS,2)-1)/NIRS.Cf.dev.fs , size(dataNIRS,2));
+
+%% Display data
+h = figure; set(h,'color','w')
+set(h,'Name',sprintf('Pre-processing step: %s', NIRS.Dt.fir.pp(preProcStep).pre))
+plot(t, dataNIRS');
 ylabel('[\muM]')
 xlabel('t [s]')
-% title(['\lambda' sprintf(' = %d nm',NIRS.Cf.dev.wl(hb))])
-title('HbO')
+switch hb
+    case 1
+        titleString = 'HbO';
+    case 2
+        titleString = 'HbR';
+    case 3
+        titleString = 'HbT';
+end
+title(titleString)
 
-%% Compute FFT
-[fftNIRS, freq] = nirs_positiveFFT(dataNIRS(NIRS.Cf.H.C.wl == hb,:)', NIRS.Cf.dev.fs);
-fftNIRSmean = mean(abs(fftNIRS), 2);
-
-%% Plot FFT
-figure(h)
-subplot(212)
-semilogy(freq, fftNIRSmean, 'k-')
-xlabel('f [Hz]')
-ylabel('Amplitude [a.u.]')
-
+%% Using clusterdata (hierarchical clustering)
+nirs_clustergram(dataNIRS);
 % EOF
