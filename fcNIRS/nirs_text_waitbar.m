639c06327e6834054a8b32b79e12abda6e956ccb
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-12-17 23:06:52 +0000

    // EGC nirs_create_vol.m - handles optional input: description nirs_create_vol_4D.m - time-series vol. data for GLM nirs_filtfilt.m - zero-phase filtering using second-order sections form to avoid numerical artifacts, like zpk form nirs_temporalBPFconfig.m - choose from different implementations nirs_temporalBPFrun.m - Performs filtering, calling nirs_filtfilt.m nirs_text_waitbar.m - Text-based waitbar script_nirs_fc01.m - functional connectivity script

---
+++ fcNIRS/nirs_text_waitbar.m
@@ -0,0 +1,47 @@
+function nirs_text_waitbar(varargin)
+% Shows a non-graphical progress indication.
+% 
+% Prior to the for-loop, you should call:
+% nirs_text_waitbar(0, 'Please wait...');
+% 
+% In each iteration of the for-loop, you should call:
+% for iX = 1:nX
+%     % Update progress bar
+%     nirs_text_waitbar(iX/nX, sprintf('Processing event %d from %d', iX, nX));
+% end
+% 
+% After finishing the for-loop, you should call
+% nirs_text_waitbar('Clear');
+% 
+%_______________________________________________________________________________
+% Copyright (C) 2013 LIOM Laboratoire d'Imagerie Optique et Moléculaire
+%                    École Polytechnique de Montréal
+%_______________________________________________________________________________
+
+if ischar(varargin{1}),
+    nirs_cumdisp;
+elseif ~varargin{1},
+    nirs_cumdisp;
+    disp(varargin{2});
+else
+    nirs_cumdisp([num2str(100*varargin{1},'%3.1f'),'% ' varargin{2}]);
+end
+end % End nirs_text_waitbar
+
+function nirs_cumdisp(txt)
+% nirs_cumdisp persistent text display
+% nirs_cumdisp;          initializes persistent display
+% nirs_cumdisp(txt);     displays persistent text
+%
+persistent oldtxt;
+
+if nargin<1,
+    oldtxt = ''; 
+    fprintf(1,'\n'); 
+else
+    fprintf(1,[repmat('\b',[1,length(oldtxt)]),'%s'],txt);
+    oldtxt = sprintf('%s',txt);
+end
+end % end nirs_cumdisp
+
+% EOF
